3a8d7b534ff04cf3939aee5492f32773b8f61650
lisun <lisun@users.noreply.github.com>
2020-10-16 14:43:42 +0800

    Update clickhouse.rb
    20.6.8.5

--- clickhouse.rb
+++ clickhouse.rb
@@ -2,11 +2,11 @@ class Clickhouse < Formula
   desc "ClickHouse is a free analytic DBMS for big data."
   homepage "https://clickhouse.yandex"
   url "https://github.com/yandex/ClickHouse.git", :tag => "v20.6.8.5-stable"
-  version "19.5.3.8"
+  version "20.6.8.5"
 
   head "https://github.com/yandex/ClickHouse.git"
 
-  depends_on "gcc@8"
+#   depends_on "gcc@8"
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "libtool" => :build
