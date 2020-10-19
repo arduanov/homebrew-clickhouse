8aa8bcbae9510cba09217e54950a056386362e44
lisun <lisun@users.noreply.github.com>
2020-10-19 20:37:49 +0800

    Update clickhouse.rb
    20.9.3.45

--- clickhouse.rb
+++ clickhouse.rb
@@ -31,7 +31,7 @@ class Clickhouse < Formula
     args = %W[
       -DENABLE_TESTS=0
       -DUSE_RDKAFKA=0
-      -DCMAKE_CXX_STANDARD=11
+      -DCMAKE_CXX_FLAGS='-std=c++11 -stdlib=libc++'
     ]
 
     mkdir "build" do
