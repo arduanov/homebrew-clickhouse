ebdc958ad4325d47038e9fbb6913bf40549afb49
lisun <lisun@users.noreply.github.com>
2020-11-03 13:32:29 +0800

    Update clickhouse.rb
    v20.10.3.30-stable

--- clickhouse.rb
+++ clickhouse.rb
@@ -1,8 +1,8 @@
 class Clickhouse < Formula
   desc "ClickHouse is a free analytic DBMS for big data."
   homepage "https://clickhouse.yandex"
-  url "https://github.com/yandex/ClickHouse.git", :tag => "v20.10.2.20-stable"
-  version "20.10.2.20"
+  url "https://github.com/yandex/ClickHouse.git", :tag => "v20.10.3.30-stable"
+  version "20.10.3.30"
 
   head "https://github.com/yandex/ClickHouse.git"
 
